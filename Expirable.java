365f9adcff4c8d8ee64ae6c5d6c501f8ecc3adf1
Youssef <youssefkaramedu@gmail.com>
2025-07-05 02:36:48 +0300

    Add Expirable.java file with it's methods

---
+++ Expirable.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public interface Expirable {
+    LocalDate getExpirationDate();
+    default boolean isExpired(){
+        LocalDate today = LocalDate.now();
+        LocalDate expiryDate = getExpirationDate();
+        return today.isAfter(expiryDate);
+    }
+}
