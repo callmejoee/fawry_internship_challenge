5bf305d8e9cc7e69b10a8fbe0ed371ce6e3372c0
Youssef <youssefkaramedu@gmail.com>
2025-07-05 15:36:21 +0300

    Create CheckoutService.java and add basic checkout logic

---
+++ service/CheckoutService.java
@@ -0,0 +1,50 @@
+package service;
+
+import model.Customer;
+import model.Expirable;
+import utils.shippingInfo;
+
+public class CheckoutService {
+
+    public void checkout(Customer customer, Cart cart) {
+
+        if (cart.isEmpty()) {
+            System.out.println("Cart is empty");
+            return;
+        }
+
+        for (CartItem item : cart.getItems()) {
+
+            if (item.getProduct() instanceof Expirable && ((Expirable) item.getProduct()).isExpired()) {
+
+                System.out.println("Item is expired: " + item.getProduct());
+                return;
+
+            }
+            item.getProduct().deductProduct(item.getQuantity());
+
+        }
+
+        double subTotal = cart.getSubTotal();
+
+        ShippingService shipping = new ShippingService();
+
+        shippingInfo shippingInfo = shipping.calculateShipping(cart.getItems());
+
+        double shippingCost = shippingInfo.getCost();
+
+        double total = shippingCost + subTotal;
+
+        if (!customer.canAfford(total)) {
+            System.out.println("Insufficient Balance");
+            return;
+        }
+
+        customer.deductBalance(total);
+
+        shippingInfo.print();
+
+
+    }
+
+}
