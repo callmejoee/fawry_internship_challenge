a7b15ba7f7243dfcc70a66c643b020eefee642d2
Youssef <youssefkaramedu@gmail.com>
2025-07-05 15:36:50 +0300

    Add the ShippingService class with implementation

---
+++ service/ShippingService.java
@@ -0,0 +1,27 @@
+package service;
+
+import model.Shippable;
+import utils.shippingInfo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShippingService {
+
+    private static final double SHIPPING_RATE = 5.0;
+
+    public shippingInfo calculateShipping(List<CartItem> items) {
+        double totalWeight = 0.0;
+        List<String> lines = new ArrayList<>();
+
+
+        for (CartItem item : items) {
+            Shippable shippable = (Shippable) item.getProduct();
+            double itemTotalWeight = shippable.getWeight() * item.getQuantity();
+            totalWeight += itemTotalWeight;
+           String line = (item.getQuantity() + "x " + item.getProduct().getName() + " " + itemTotalWeight);
+           lines.add(line);
+        }
+        double shippingCost = SHIPPING_RATE * totalWeight;
+        return new shippingInfo(lines, totalWeight, shippingCost);
+    }
+}
