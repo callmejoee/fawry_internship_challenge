536d40159be1355a05f137874a6803e9f7633a9c
Youssef <youssefkaramedu@gmail.com>
2025-07-05 11:23:43 +0300

    Add MobileScratchCard.java

---
+++ model/MobileScratchCard.java
@@ -0,0 +1,8 @@
+package model;
+
+public class MobileScratchCard extends Product {
+
+    public MobileScratchCard(String name, Double price, Integer quantity) {
+        super(name, price, quantity);
+    }
+}
