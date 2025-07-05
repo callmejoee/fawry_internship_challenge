ddfe296d94ab26f3a44eccff6c0a63326ded9396
Youssef <youssefkaramedu@gmail.com>
2025-07-05 19:07:02 +0300

    Add Base fee

--- service/ShippingService.java
+++ service/ShippingService.java
@@ -2,12 +2,16 @@ package service;
 
 import model.Shippable;
 import utils.ShippingInfo;
+import utils.WeightUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShippingService {
 
     private static final double SHIPPING_RATE = 5.0;
+    private static final double BASE_FEE = 10.0;
+
 
     public ShippingInfo calculateShipping(List<CartItem> items) {
         double totalWeight = 0.0;
@@ -18,10 +22,10 @@ public class ShippingService {
             Shippable shippable = (Shippable) item.getProduct();
             double itemTotalWeight = shippable.getWeight() * item.getQuantity();
             totalWeight += itemTotalWeight;
-           String line = (item.getQuantity() + "x " + item.getProduct().getName() + " " + itemTotalWeight);
+           String line = (item.getQuantity() + "x " + item.getProduct().getName() + " " + WeightUtils.printWeight(itemTotalWeight));
            lines.add(line);
         }
-        double shippingCost = SHIPPING_RATE * totalWeight;
+        double shippingCost = BASE_FEE + SHIPPING_RATE * WeightUtils.toKilograms(totalWeight);
         return new ShippingInfo(lines, totalWeight, shippingCost);
     }
 }
